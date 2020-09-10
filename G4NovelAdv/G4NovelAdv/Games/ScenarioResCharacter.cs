43235826d2f2da7e00509ece7bc8db0203761b8a
stackprobe <stackprobes@gmail.com>
2020-09-10 09:56:08 +0900

    Ymir

--- G4NovelAdv/G4NovelAdv/Games/ScenarioResCharacter.cs
+++ G4NovelAdv/G4NovelAdv/Games/ScenarioResCharacter.cs
@@ -27,22 +27,6 @@ namespace Charlotte.Games
 
 		private ScenarioResCharacter()
 		{
-#if false // del @ 2020.5.24
-			this.Name2Picture.Add("01a", Ground.I.Picture.Chara_01_A);
-			this.Name2Picture.Add("01d", Ground.I.Picture.Chara_01_D);
-			this.Name2Picture.Add("01h", Ground.I.Picture.Chara_01_H);
-			this.Name2Picture.Add("04a", Ground.I.Picture.Chara_04_A);
-			this.Name2Picture.Add("06a", Ground.I.Picture.Chara_06_A);
-			this.Name2Picture.Add("07a", Ground.I.Picture.Chara_07_A);
-			this.Name2Picture.Add("13a", Ground.I.Picture.Chara_13_A);
-			this.Name2Picture.Add("13b", Ground.I.Picture.Chara_13_B);
-			this.Name2Picture.Add("13c", Ground.I.Picture.Chara_13_C);
-			this.Name2Picture.Add("13d", Ground.I.Picture.Chara_13_D);
-			this.Name2Picture.Add("13e", Ground.I.Picture.Chara_13_E);
-			this.Name2Picture.Add("13f", Ground.I.Picture.Chara_13_F);
-			this.Name2Picture.Add("13g", Ground.I.Picture.Chara_13_G);
-			this.Name2Picture.Add("13h", Ground.I.Picture.Chara_13_H);
-#endif
 		}
 
 		private const string CHARA_FILE_PREFIX = "Etoile\\G4NovelAdv\\わたおきば\\";
@@ -50,11 +34,7 @@ namespace Charlotte.Games
 
 		public DDPicture GetPicture(string name)
 		{
-#if true
 			return DDCResource.GetPicture(CHARA_FILE_PREFIX + name + CHARA_FILE_SUFFIX);
-#else // del @ 2020.5.24
-			return this.Name2Picture[name];
-#endif
 		}
 	}
 }
