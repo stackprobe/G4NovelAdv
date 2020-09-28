99e2ccdbd940d1abfa953ba20e4ea0deaf1673f6
stackprobe <stackprobes@gmail.com>
2020-09-28 16:47:28 +0900

    Enceladus

--- G4NovelAdv/G4NovelAdv/ResourcePicture.cs
+++ G4NovelAdv/G4NovelAdv/ResourcePicture.cs
@@ -71,10 +71,10 @@ namespace Charlotte
 		public DDPicture 東北ずん子11 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\からい\東北ずん子\zunko11.png");
 
 		public DDPicture 未確認飛行ゆかりん_UFO = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO.png");
-		public DDPicture 未確認飛行ゆかりん_UFO_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO_うしろ.png");
-		public DDPicture 未確認飛行ゆかりん_UFO_光 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO_光 .png");
-		public DDPicture 未確認飛行ゆかりん_UFO_手付き = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO_手付き.png");
-		public DDPicture 未確認飛行ゆかりん_UFO_手付き_光 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO_手付き_光.png");
+		public DDPicture 未確認飛行ゆかりん_UFO_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO(うしろ.png");
+		public DDPicture 未確認飛行ゆかりん_UFO_光 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO(光.png");
+		public DDPicture 未確認飛行ゆかりん_UFO_手付き = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO(手付き.png");
+		public DDPicture 未確認飛行ゆかりん_UFO_手付き_光 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\UFO(手付き(光.png");
 		public DDPicture 未確認飛行ゆかりん_あたま0000 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\あたま0000.png");
 		public DDPicture 未確認飛行ゆかりん_あたま0001 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\あたま0001.png");
 		public DDPicture 未確認飛行ゆかりん_あたま0002 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\あたま0002.png");
@@ -82,10 +82,10 @@ namespace Charlotte
 		public DDPicture 未確認飛行ゆかりん_あたま0004 = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\あたま0004.png");
 		public DDPicture 未確認飛行ゆかりん_からだ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\からだ.png");
 		public DDPicture 未確認飛行ゆかりん_てのりゆかり = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\てのりゆかり.png");
-		public DDPicture 未確認飛行ゆかりん_まかろん_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\まかろん_うしろ.png");
-		public DDPicture 未確認飛行ゆかりん_まかろん_まえ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\まかろん_まえ.png");
-		public DDPicture 未確認飛行ゆかりん_もみあげ_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\もみあげ_うしろ.png");
-		public DDPicture 未確認飛行ゆかりん_もみあげ_まえ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\もみあげ_まえ.png");
+		public DDPicture 未確認飛行ゆかりん_まかろん_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\まかろん(うしろ.png");
+		public DDPicture 未確認飛行ゆかりん_まかろん_まえ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\まかろん(まえ.png");
+		public DDPicture 未確認飛行ゆかりん_もみあげ_うしろ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\もみあげ(うしろ.png");
+		public DDPicture 未確認飛行ゆかりん_もみあげ_まえ = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\もみあげ(まえ.png");
 		public DDPicture 未確認飛行ゆかりん_キャッチャー = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\キャッチャー.png");
 		public DDPicture 未確認飛行ゆかりん_髪飾り = DDPictureLoaders.Standard(@"Etoile\G4NovelAdv\立ち絵\未確認飛行ゆかりん\UFyukari\髪飾り.png");
 
